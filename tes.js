8542bb75d8fe037fcd7a8f5ff589d79fe94d3b5e
komangsuryasedana <komangsuryasedana@mail.com>
2024-03-11 16:54:12 +0800

    fix: fixed file in packagejson

---
+++ tes.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: "api-chat-app",
+//       script: "./server.js",
+//       env: {
+//         DB_USER: "postgres",
+//         DB_PASSWORD: "komang26",
+//         DB_NAME: "real_timechat",
+//         DB_HOST: "localhost",
+//         SECRET_JWT: "kdmk:d,w;a;a,dlf,dv,vd",
+//       },
+//     },
+//   ],
+// };
